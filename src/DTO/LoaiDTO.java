a2d1123cd63d2d3a65909861e499231e28465ba8
Khanh Tang <quockhanhtanghuynh@gmail.com>
2026-01-16 15:18:57 +0700

    Create all DTO class

---
+++ src/DTO/LoaiDTO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author quock
+ */
+public class LoaiDTO {
+    private String maLoai;
+    private String tenLoai;
+    private boolean trangThaiXoa;
+
+    public LoaiDTO() {}
+}
